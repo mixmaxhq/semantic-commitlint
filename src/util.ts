97e8377f99ead56e63cfc75b6f94acabbef536a5
Eli Skeggs <eli@mixmax.com>
2019-11-04 11:35:24 -0800

    feat: initial fork release
    - remove cli and local functionality
    - remove typescript
    - use standard Mixmax shared modules setup

--- src/util.ts
+++
@@ -1,6 +0,0 @@
-import * as readPkgUp from 'read-pkg-up';
-
-export async function getSemanticCommitlintConfig() {
-  const { pkg } = await readPkgUp();
-  return pkg && pkg.semanticCommitlint ? pkg.semanticCommitlint : {};
-}
